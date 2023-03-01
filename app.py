333305c33117d29f85b555e5ab0008a6b1969e93
Thomashallberg <112643313+Thomashallberg@users.noreply.github.com>
2023-03-01 10:15:28 +0100

    debug borta

--- app.py
+++ app.py
@@ -297,7 +297,7 @@ if __name__  == "__main__":
         print("Startar seed")
         seed_user(app, db)
         print("Seeding done")
-        app.run(host='0.0.0.0', debug=True)
+        app.run(host='0.0.0.0')
 
         # while True:
         #     print("1. Create")
